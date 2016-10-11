b8e3bff38a310ea3491c8b33fbba1983f081a96e
Yann <yannan0911@hotmail.com>
2016-10-11 19:44:39 +0800

    Ambari scripts flow check_dead - new: 检查 DEAD DATANODE 流程。 Ambari conf conf - change: 1.变更主目录结构，增加 scripts/flow tmp/flow logs 目录，及初始化目录结构的代码；2.增加关于 check_dead_dn 流程的配置。

---
+++ Ambari/scripts/flow/check_dead_dn.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+SCRIPT_FLOW_DIR=${SCRIPT_FLOW_DIR:-`pwd`}
+SCRIPTS_DIR=${SCRIPTS_DIR:-$SCRIPT_FLOW_DIR/..}
+. ../../conf/conf
+
+$HDFS dfsadmin -report > $DFSADMIN_REPORT_TMP
+if [ -s $DEAD_DN_LIST_TMP ]
+then
+  cp -a $DEAD_DN_LIST_TMP{,.$SEC1970}
+fi
+grep -E '^(Name|Decommission Status|Last contact)' $DFSADMIN_REPORT_TMP | \
+  awk '{
+    if($0~/^Last contact/) {
+      $1=""
+      $2=""
+      system("date +%s -d \x027"$0"\x027")
+      print ""
+    }
+    else {
+      split($NF,content,":")
+      printf content[1]" "
+    }
+  }' | awk '/Normal/{
+    if('$SEC1970'-$3>'$THRESHOLD_SECOND') {
+      print $1
+    }
+  }' > $DEAD_DN_LIST_TMP
+
+cd $SCRIPTS_DIR
+COMMAND='ps aux|grep -E "TaskTracker|DataNode"|grep -v grep|awk "{print \$2}"|xargs kill -9;sleep 5;'
+COMMAND=$COMMAND'sudo -u work /search/work/hadoop-envir/hadoop-datanode/bin/start-datanode.sh;'
+COMMAND=$COMMAND'sudo -u work /search/work/hadoop-envir/hadoop-tasktracker/bin/start-tasktracker.sh'
+sh ssh.sh "$COMMAND" $DEAD_DN_LIST_TMP
+cd - > /dev/null 2>&1
