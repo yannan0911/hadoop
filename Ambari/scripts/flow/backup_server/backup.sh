2e35211598f42a45b70aa2ccd2711d25764b359d
Yann <yannan0911@hotmail.com>
2017-01-05 16:16:46 +0800

    Ambari conf conf - change: 增加系统命令绝对路径。 Ambari conf conf_special - change: 增加数据、tasktracker 及 datanode 目录变量。 Ambari scripts flow backup_server backup - change: 增加打包时 exclude 的内容。

--- Ambari/scripts/flow/backup_server/backup.sh
+++ Ambari/scripts/flow/backup_server/backup.sh
@@ -6,11 +6,29 @@ SCRIPTS_DIR=${SCRIPTS_DIR:-$SCRIPT_FLOW_BACKUP_SERVER_DIR/../..}
 echo
 print_time_tag
 
-tar --exclude $JOBTRACKER_DIR/logs --exclude $NAMENODE_DIR/logs --exclude $BACKUP_SERVER_DATA_DIR -zcf $BACKUP_SERVER_DATA_DIR/backup-$HADOOP_PLATFORM-$BACKUP_TIME.tar.gz $HADOOP_WORK_DIR
+BACKUP_EXCLUDE_PARAM=" \
+  --exclude $JOBTRACKER_DIR/logs \
+  --exclude $NAMENODE_DIR/logs \
+  --exclude $TASKTRACKER_DIR/logs \
+  --exclude $DATANODE_DIR/logs \
+  --exclude $HADOOP_DATA_DIR/data01 \
+  --exclude $HADOOP_DATA_DIR/data02 \
+  --exclude $HADOOP_DATA_DIR/data03 \
+  --exclude $HADOOP_DATA_DIR/data04 \
+  --exclude $HADOOP_DATA_DIR/data05 \
+  --exclude $HADOOP_DATA_DIR/data06 \
+  --exclude $HADOOP_DATA_DIR/data07 \
+  --exclude $HADOOP_DATA_DIR/data08 \
+  --exclude $HADOOP_DATA_DIR/data09 \
+  --exclude $HADOOP_DATA_DIR/data10 \
+  --exclude $HADOOP_DATA_DIR/data11 \
+  --exclude $HADOOP_DATA_DIR/data12 \
+  --exclude $BACKUP_SERVER_DATA_DIR"
+tar $BACKUP_EXCLUDE_PARAM -zcf $BACKUP_SERVER_DATA_DIR/backup-${HADOOP_PLATFORM}_$SERVER_IP-$BACKUP_TIME.tar.gz $HADOOP_WORK_DIR/*
 if [ $? -eq 0 ]
 then
   touch $BACKUP_SERVER_DATA_DIR/$BACKUP_TIME.done
 fi
 
-rm -f $BACKUP_SERVER_DATA_DIR/backup-$HADOOP_PLATFORM-${DEL_DAY}*.tar.gz
+rm -f $BACKUP_SERVER_DATA_DIR/backup-${HADOOP_PLATFORM}_$SERVER_IP-${DEL_DAY}*.tar.gz
 rm -f $BACKUP_SERVER_DATA_DIR/${DEL_DAY}*.done
