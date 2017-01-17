b963b50be7da84dd1bfeb24f0853ce971b83e84c
Yann <yannan0911@hotmail.com>
2017-01-17 17:35:25 +0800

    Ambari conf conf - change: 1.增加 public 及 backup_server 变量。 Ambari scripts func - change: 1.增加 mail_tool check_value_add postgre_dump 函数。 Ambari scripts flow backup_server backup - change: 1.修改报警邮件发送机制；2.Ambari 备份时增加失败判断机制。

--- Ambari/scripts/flow/backup_server/backup.sh
+++ Ambari/scripts/flow/backup_server/backup.sh
@@ -5,6 +5,9 @@ SCRIPTS_DIR=${SCRIPTS_DIR:-$SCRIPT_FLOW_BACKUP_SERVER_DIR/../..}
 
 echo
 print_time_tag
+BACK_DONE="$BACKUP_SERVER_DATA_DIR/$BACKUP_TIME.done"
+BACK_TAR="$BACKUP_SERVER_DATA_DIR/backup-${HADOOP_PLATFORM}_$SERVER_IP-$BACKUP_TIME.tar.gz"
+rm -f $BACK_DONE $BACK_TAR
 
 case $PLATFORM_TYPE in
   1)
@@ -26,37 +29,66 @@ case $PLATFORM_TYPE in
       --exclude $HADOOP_DATA_DIR/data11 \
       --exclude $HADOOP_DATA_DIR/data12 \
       --exclude $BACKUP_SERVER_DATA_DIR"
-    tar $BACKUP_EXCLUDE_PARAM -zcf $BACKUP_SERVER_DATA_DIR/backup-${HADOOP_PLATFORM}_$SERVER_IP-$BACKUP_TIME.tar.gz $HADOOP_WORK_DIR/*
+    tar $BACKUP_EXCLUDE_PARAM -zcf $BACK_TAR $HADOOP_WORK_DIR/*
     if [ $? -eq 0 ]
     then
-      touch $BACKUP_SERVER_DATA_DIR/$BACKUP_TIME.done
+      touch $BACK_DONE
     else
-      sh $MAIL_TOOL -t $ADMIN_MAIL -s "[Hadoop_Report] $HADOOP_PLATFORM backup tar error" -c " " > /dev/null
+      MAIL_CONTENT="$MAIL_CONTENT TAR"
     fi
 
     rm -f $BACKUP_SERVER_DATA_DIR/backup-${HADOOP_PLATFORM}_$SERVER_IP-${DEL_DAY}*.tar.gz
     rm -f $BACKUP_SERVER_DATA_DIR/${DEL_DAY}*.done
   ;;
   2)
-    export PGPASSWORD=$PG_PASSWORD_AMBARI
-    $PG_DUMP -U ambari ambari > $TMP_DIR/ambari.sql
-    export PGPASSWORD=$PG_PASSWORD_AMBARIRCA
-    $PG_DUMP -U mapred ambarirca > $TMP_DIR/ambarirca.sql
+    CHECK_TAG=0
+    PS_LIST=$(ps aux | grep -E "$KEY_WORD_AMBARI_SERVER|$KEY_WORD_KERBEROS")
+    CHECK=$(echo "$PS_LIST" | grep "$KEY_WORD_AMBARI_SERVER" | grep -v grep)
+    if [ -n "$CHECK" ]
+    then
+      postgre_dump $PG_PASSWORD_AMBARI $PG_USER_AMBARI $PG_DATABASE_AMBARI
+      postgre_dump $PG_PASSWORD_AMBARIRCA $PG_USER_AMBARIRCA $PG_DATABASE_AMBARIRCA
 
-    tar zcf $BACKUP_SERVER_DATA_DIR/backup-${HADOOP_PLATFORM}_$SERVER_IP-$BACKUP_TIME.tar.gz $TMP_DIR/ambari*.sql
+      if [ $CHECK_TAG -ne 0 ]
+      then
+        MAIL_CONTENT="$MAIL_CONTENT PG_DUMP"
+      fi
+    fi
+
+    CHECK=$(echo "$PS_LIST" | grep "$KEY_WORD_KERBEROS" | grep -v grep)
+    if [ -n "$CHECK" ]
+    then
+      $KDB5_UTIL dump $TMP_DIR/$KRB_DUMP_FILE
+      if [ ! -f $TMP_DIR/$KRB_DUMP_FILE_OK ]
+      then
+        MAIL_CONTENT="$MAIL_CONTENT KDB5_DUMP"
+      fi
+
+      cp -a $KRB_ROOT_DIR $TMP_DIR/${BACKUP_FILE_TAG}_$KRB_DIR_NAME
+      cp -a $KRB_ETC_CONF $TMP_DIR/${BACKUP_FILE_TAG}_$KRB_DIR_NAME
+      if [ $? -ne 0 ]
+      then
+        MAIL_CONTENT="$MAIL_CONTENT CP_KRB_FILE"
+      fi
+    fi
+
+    tar zcf $BACK_TAR $TMP_DIR/${BACKUP_FILE_TAG}_*
     if [ $? -eq 0 ]
     then
-      touch $BACKUP_SERVER_DATA_DIR/$BACKUP_TIME.done
+      touch $BACK_DONE
     else
-      sh $MAIL_TOOL -t $ADMIN_MAIL -s "[Hadoop_Report] $HADOOP_PLATFORM backup tar error" -c " " > /dev/null
+      MAIL_CONTENT="$MAIL_CONTENT TAR_KRB_FILE"
     fi
 
     rm -f $BACKUP_SERVER_DATA_DIR/backup-${HADOOP_PLATFORM}_$SERVER_IP-${DEL_DAY}*.tar.gz
     rm -f $BACKUP_SERVER_DATA_DIR/${DEL_DAY}*.done
   ;;
   *)
-    sh $MAIL_TOOL -t $ADMIN_MAIL -s "[Hadoop_Report] $HADOOP_PLATFORM backup PLATFORM_TYPE error" -c " " > /dev/null
+    MAIL_CONTENT="$MAIL_CONTENT PLATFORM_TYPE"
   ;;
 esac
 
-rm -f temp_wget_mail
+if [ -n "${MAIL_CONTENT// /}" ]
+then
+  mail_tool "backup error" "$MAIL_CONTENT"
+fi
--- Ambari/scripts/func.sh
+++ Ambari/scripts/func.sh
@@ -1,3 +1,32 @@
 function print_time_tag() {
   date +"%Y%m%d_%H%M"
 }
+
+function mail_tool() {
+  MAIL_TITAL_TAIL="$1"
+  if [ -z "$2" ]
+  then
+    M_CONTENT="."
+  else
+    M_CONTENT="$2"
+  fi
+  sh $MAIL_TOOL -t $ADMIN_MAIL -s "$MAIL_TITAL $MAIL_TITAL_TAIL" -c "$M_CONTENT" > /dev/null
+  rm -f temp_wget_mail
+}
+
+function check_value_add() {
+  if [ $? -ne 0 ]
+  then
+    CHECK_TAG=$1
+    CHECK_TAG=$((CHECK_TAG+1))
+  fi
+}
+
+function postgre_dump() {
+  export PGPASSWORD=$1
+  PG_USER=$2
+  PG_DATABASE=$3
+  local BACKUP_SQL="$TMP_DIR/${BACKUP_FILE_TAG}_$PG_DATABASE.sql"
+  $PG_DUMP -U $PG_USER $PG_DATABASE > "$BACKUP_SQL"
+  check_value_add $CHECK_TAG
+}
