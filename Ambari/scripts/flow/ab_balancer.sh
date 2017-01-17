fc7aa43bbfb69448aef09ab98e6e2ec116d2a541
Yann <yannan0911@hotmail.com>
2017-01-17 17:41:56 +0800

    Ambari scripts flow ab_balancer - change: 1.修改一处错误；2.修改执行时日志收集方式。

--- Ambari/scripts/flow/ab_balancer.sh
+++ Ambari/scripts/flow/ab_balancer.sh
@@ -3,9 +3,9 @@ SCRIPT_FLOW_DIR=${SCRIPT_FLOW_DIR:-`pwd`}
 SCRIPTS_DIR=${SCRIPTS_DIR:-$SCRIPT_FLOW_DIR/..}
 . ../../conf/conf
 
-check=$(ps aux | 'grep org.apache.hadoop.hdfs.server.balancer.Balancer$')
+check=$(ps aux | grep 'org.apache.hadoop.hdfs.server.balancer.Balancer$')
 if [ -z "$check" ]
 then
   /usr/bin/kinit -kt /etc/security/keytabs/hdfs.headless.keytab hdfs
-  hdfs balancer
+  nohup hdfs balancer > logs/balancer.out 2>&1 &
 fi
