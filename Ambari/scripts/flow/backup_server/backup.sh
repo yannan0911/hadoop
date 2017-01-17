aae07182815bd07e9db87af4ee26829b07c1acaf
Yann <yannan0911@hotmail.com>
2017-01-17 09:49:51 +0800

    Ambari conf conf - change: 增加 postgres 数据库备份命令变量。 Ambari conf conf_specail - change: 增加 postgres 数据库密码。 Ambari scripts flow backup_server backup  - change: 1.增加错误时的邮件报警；2.增加兼容 Ambari 的备份代码。

--- Ambari/scripts/flow/backup_server/backup.sh
+++ Ambari/scripts/flow/backup_server/backup.sh
@@ -6,29 +6,57 @@ SCRIPTS_DIR=${SCRIPTS_DIR:-$SCRIPT_FLOW_BACKUP_SERVER_DIR/../..}
 echo
 print_time_tag
 
-BACKUP_EXCLUDE_PARAM=" \
-  --exclude $JOBTRACKER_DIR/logs \
-  --exclude $NAMENODE_DIR/logs \
-  --exclude $TASKTRACKER_DIR/logs \
-  --exclude $DATANODE_DIR/logs \
-  --exclude $HADOOP_DATA_DIR/data01 \
-  --exclude $HADOOP_DATA_DIR/data02 \
-  --exclude $HADOOP_DATA_DIR/data03 \
-  --exclude $HADOOP_DATA_DIR/data04 \
-  --exclude $HADOOP_DATA_DIR/data05 \
-  --exclude $HADOOP_DATA_DIR/data06 \
-  --exclude $HADOOP_DATA_DIR/data07 \
-  --exclude $HADOOP_DATA_DIR/data08 \
-  --exclude $HADOOP_DATA_DIR/data09 \
-  --exclude $HADOOP_DATA_DIR/data10 \
-  --exclude $HADOOP_DATA_DIR/data11 \
-  --exclude $HADOOP_DATA_DIR/data12 \
-  --exclude $BACKUP_SERVER_DATA_DIR"
-tar $BACKUP_EXCLUDE_PARAM -zcf $BACKUP_SERVER_DATA_DIR/backup-${HADOOP_PLATFORM}_$SERVER_IP-$BACKUP_TIME.tar.gz $HADOOP_WORK_DIR/*
-if [ $? -eq 0 ]
-then
-  touch $BACKUP_SERVER_DATA_DIR/$BACKUP_TIME.done
-fi
+case $PLATFORM_TYPE in
+  1)
+    BACKUP_EXCLUDE_PARAM=" \
+      --exclude $JOBTRACKER_DIR/logs \
+      --exclude $NAMENODE_DIR/logs \
+      --exclude $TASKTRACKER_DIR/logs \
+      --exclude $DATANODE_DIR/logs \
+      --exclude $HADOOP_DATA_DIR/data01 \
+      --exclude $HADOOP_DATA_DIR/data02 \
+      --exclude $HADOOP_DATA_DIR/data03 \
+      --exclude $HADOOP_DATA_DIR/data04 \
+      --exclude $HADOOP_DATA_DIR/data05 \
+      --exclude $HADOOP_DATA_DIR/data06 \
+      --exclude $HADOOP_DATA_DIR/data07 \
+      --exclude $HADOOP_DATA_DIR/data08 \
+      --exclude $HADOOP_DATA_DIR/data09 \
+      --exclude $HADOOP_DATA_DIR/data10 \
+      --exclude $HADOOP_DATA_DIR/data11 \
+      --exclude $HADOOP_DATA_DIR/data12 \
+      --exclude $BACKUP_SERVER_DATA_DIR"
+    tar $BACKUP_EXCLUDE_PARAM -zcf $BACKUP_SERVER_DATA_DIR/backup-${HADOOP_PLATFORM}_$SERVER_IP-$BACKUP_TIME.tar.gz $HADOOP_WORK_DIR/*
+    if [ $? -eq 0 ]
+    then
+      touch $BACKUP_SERVER_DATA_DIR/$BACKUP_TIME.done
+    else
+      sh $MAIL_TOOL -t $ADMIN_MAIL -s "[Hadoop_Report] $HADOOP_PLATFORM backup tar error" -c " " > /dev/null
+    fi
 
-rm -f $BACKUP_SERVER_DATA_DIR/backup-${HADOOP_PLATFORM}_$SERVER_IP-${DEL_DAY}*.tar.gz
-rm -f $BACKUP_SERVER_DATA_DIR/${DEL_DAY}*.done
+    rm -f $BACKUP_SERVER_DATA_DIR/backup-${HADOOP_PLATFORM}_$SERVER_IP-${DEL_DAY}*.tar.gz
+    rm -f $BACKUP_SERVER_DATA_DIR/${DEL_DAY}*.done
+  ;;
+  2)
+    export PGPASSWORD=$PG_PASSWORD_AMBARI
+    $PG_DUMP -U ambari ambari > $TMP_DIR/ambari.sql
+    export PGPASSWORD=$PG_PASSWORD_AMBARIRCA
+    $PG_DUMP -U mapred ambarirca > $TMP_DIR/ambarirca.sql
+
+    tar zcf $BACKUP_SERVER_DATA_DIR/backup-${HADOOP_PLATFORM}_$SERVER_IP-$BACKUP_TIME.tar.gz $TMP_DIR/ambari*.sql
+    if [ $? -eq 0 ]
+    then
+      touch $BACKUP_SERVER_DATA_DIR/$BACKUP_TIME.done
+    else
+      sh $MAIL_TOOL -t $ADMIN_MAIL -s "[Hadoop_Report] $HADOOP_PLATFORM backup tar error" -c " " > /dev/null
+    fi
+
+    rm -f $BACKUP_SERVER_DATA_DIR/backup-${HADOOP_PLATFORM}_$SERVER_IP-${DEL_DAY}*.tar.gz
+    rm -f $BACKUP_SERVER_DATA_DIR/${DEL_DAY}*.done
+  ;;
+  *)
+    sh $MAIL_TOOL -t $ADMIN_MAIL -s "[Hadoop_Report] $HADOOP_PLATFORM backup PLATFORM_TYPE error" -c " " > /dev/null
+  ;;
+esac
+
+rm -f temp_wget_mail
